0115a457b4eccd48bb22341dc74ad9f562ec9b89
Sachin  Kushwaha <sachinkumarkushwaha991071@gmail.com>
2026-02-28 15:21:02 +0530

    Delete Mini Insta Project directory

--- Mini Insta Project/Backend/src/db/db.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const connectDb = async () => {
-  await mongoose.connect(process.env.MONGODB_URI);
-
-  console.log("Connected to DB");
-};
-
-module.exports = connectDb;
